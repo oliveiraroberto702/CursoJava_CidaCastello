41f9a39fd43a3877a021784b2de039dca937d365
Roberto Vasconcellos de Oliveira <robertvascol702@gmail.com>
2025-09-01 13:19:58 -0300

    commit 5º acréscimo aula009 - comentarios

---
+++ Aula009_Comentrarios/src/curso/Comentarios.java
@@ -0,0 +1,27 @@
+package curso;
+
+/**
+*
+* @author  roberto oliveira
+* @version 1.0
+* @since 01/2023
+*
+*/
+public class Comentarios {
+	public static void main (String[] args) {
+     // comentário em linha
+		
+	/*
+	 * comentário em bloco
+	 */
+	System.out.println("Primeira linha"); // comentario
+	/*
+	 	System.out.println("Segunda linha");
+	    System.out.println("Teceira linha");
+	
+	*/	
+		System.out.println("Depois do bloco comentado");
+		
+	}
+
+}
