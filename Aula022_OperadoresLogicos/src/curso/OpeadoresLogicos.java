9a1ef8ec7212c59d48d7bbd78382e8a39e011114
Roberto Vasconcellos de Oliveira <robertvascol702@gmail.com>
2025-09-09 22:11:25 -0300

    Aula 022 - Operadores Lógico não ! - ou || = e &&

---
+++ Aula022_OperadoresLogicos/src/curso/OpeadoresLogicos.java
@@ -0,0 +1,20 @@
+package curso;
+
+public class OpeadoresLogicos {
+
+	public static void main(String[] args) {
+		boolean  resultado;
+		resultado = !((4+3) != 17);
+		
+		System.out.println("Primeiro = " + resultado);
+		
+		resultado = ( 4 > 3) && (4 != 2);
+		System.out.println("Segundo = " + resultado);
+		
+		resultado = ( 2 * 3 > Math.pow(2,3)) || (2 *3 == 6);
+		System.out.println("Terceiro = " + resultado);
+		
+
+	}
+
+}
