267d39a3be26c36c16426959e5f41a049b6050c2
Roberto Vasconcellos de Oliveira <robertvascol702@gmail.com>
2025-09-13 19:36:09 -0300

    Aula028 Condiciomal Aninhada if else if else if else

---
+++ Aula028_CondicionalAninhada/src/curso/CondicionalAninhada.java
@@ -0,0 +1,18 @@
+package curso;
+
+public class CondicionalAninhada {
+	
+	public static void main(String[] args) {
+		
+		int x =	6,  y = 6;
+		if (x > 5)  {
+			if (y > 5) { 
+				System.out.println("x e y são > 5");
+			}
+		}else {
+			System.out.println("x é <= 5 ");
+		
+		}
+	}
+
+}
---
+++ Aula028_CondicionalAninhada/src/curso/CondicionalAninhada2.java
@@ -0,0 +1,19 @@
+package curso;
+
+public class CondicionalAninhada2 {
+	public static void main(String[] args) {
+		int primeiro = 8 ;
+		int segundo = 3;
+		
+		if (primeiro == segundo) {
+			System.out.println("Os números são iguais");
+		} else if (primeiro > segundo) {
+				System.out.println("primeiro é maior");
+		} else {
+				System.out.println("segundo é maior");
+		}
+			
+		
+	}
+
+}
