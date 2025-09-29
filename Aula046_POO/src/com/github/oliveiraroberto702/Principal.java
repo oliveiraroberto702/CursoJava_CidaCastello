55e2638eb0bff04b7b0a2af0520894b28efb940e
Roberto Vasconcellos de Oliveira <robertvascol702@gmail.com>
2025-09-29 08:59:21 -0300

    Aula 046 - POO - Classes e Objetos - UML

---
+++ Aula046_POO/src/com/github/oliveiraroberto702/Principal.java
@@ -0,0 +1,11 @@
+package com.github.oliveiraroberto702;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, minha primeira classe!");
+		
+
+	}
+
+}
---
+++ Aula046_POO/src/com/github/oliveiraroberto702/modelo/Carro.java
@@ -0,0 +1,5 @@
+package com.github.oliveiraroberto702.modelo;
+
+public class Carro {
+
+}
