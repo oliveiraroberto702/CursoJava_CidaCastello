5cfd63f5b01a674f62e1423436ca1f2c99bb8993
Roberto Vasconcellos de Oliveira <robertvascol702@gmail.com>
2025-09-29 09:41:07 -0300

    Aula 046 - POO - Classes e Objetos - UML - instancia de objeto - Atributos

--- Aula046_POO/src/com/github/oliveiraroberto702/Principal.java
+++ Aula046_POO/src/com/github/oliveiraroberto702/Principal.java
@@ -1,11 +1,15 @@
 package com.github.oliveiraroberto702;
+// imports
+import com.github.oliveiraroberto702.modelo.Carro;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, minha primeira classe!");
+		// System.out.println("Hello, minha primeira classe!");
+		// criando objeto do tipo Carro
+		Carro carro = new Carro();
 		
-
+		
 	}
 
 }
--- Aula046_POO/src/com/github/oliveiraroberto702/modelo/Carro.java
+++ Aula046_POO/src/com/github/oliveiraroberto702/modelo/Carro.java
@@ -1,5 +1,17 @@
 package com.github.oliveiraroberto702.modelo;
-
+// declaração da classe
 public class Carro {
 
+	// variáveis de instância (atributos, campos)
+	// tipo - nome da variaável de instância (atributo)
+	
+	String marca;
+	String modelo;
+	int anoFabricacao;
+	String cor;
+	double preco;
+	
+	
+	
+	// métodos
 }
