3d3d3466cec0849128fc160dc731c30a2d67bfa2
Roberto Vasconcellos de Oliveira <robertvascol702@gmail.com>
2025-09-30 09:23:13 -0300

    Aula 48 - POO atributos Pessoa Fisica

---
+++ Aula048_ProjetoPessoaFisica/src/com/github/oliveiraroberto702/Main.java
@@ -0,0 +1,40 @@
+package com.github.oliveiraroberto702;
+
+import com.github.oliveiraroberto702.objeto.PessoaFisica;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// instancia pessoa fisica
+		PessoaFisica pf = new PessoaFisica();
+		// pf 1
+		pf.nome = "Marcia";
+		pf.sobrenome = "Baccarat";
+		pf.cpf = "123456-78";
+		pf.telefone = "988211908" ;
+		pf.email = "mbvascol@hotmail.com";
+		PessoaFisica pf2 = new PessoaFisica();
+		// pf 2
+		pf2.nome = "José";
+		pf2.sobrenome = "Baccarat";
+		pf2.cpf = "223456-12";
+		pf2.telefone = "727211908" ;
+		pf2.email = "JoseBaccarat@gmail.com";
+		System.out.println("Nome: " + pf.nome);
+		System.out.println("Sobrenome: " + pf.sobrenome);
+		System.out.println("CPF: " + pf.cpf);
+		System.out.println("Telefone: " + pf.telefone);
+		System.out.println("Email: " + pf.email);
+		System.out.println("Nome: " + pf2.nome);
+		System.out.println("Sobrenome: " + pf2.sobrenome);
+		System.out.println("CPF: " + pf2.cpf);
+		System.out.println("Telefone: " + pf2.telefone);
+		System.out.println("Email: " + pf2.email);
+		
+		
+		
+		
+
+	}
+
+}
---
+++ Aula048_ProjetoPessoaFisica/src/com/github/oliveiraroberto702/objeto/PessoaFisica.java
@@ -0,0 +1,15 @@
+package com.github.oliveiraroberto702.objeto;
+
+public class PessoaFisica {
+	
+	// atributos
+	public String nome;
+	public String sobrenome;
+	public String cpf;
+	public String telefone;
+	public String email;
+
+	// métodos
+	
+
+}
